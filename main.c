920a1035c581a3f8e6ba1fa0c2600870e486a834
RAJA2023H <hemmanyrajae@gmail.com>
2023-09-22 19:24:21 -0700

    exit_stat

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ int command_failed = 0;
  */
 int main(int ac, char **av)
 {
-	int count = 0, i = 0, j = 0, sp_count = 0,
+	int count = 0, i = 0, j = 0, sp_count = 0, exit_stat = 0,
 	    (*validate_char)(char) = check_char, valid_chars = 0;
 	char *line = NULL, **pieces_array = NULL, *path = NULL;
 	size_t buffer = 0;
